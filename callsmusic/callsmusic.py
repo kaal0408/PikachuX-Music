0415f48275867fbb05f9e706e0f1d593df497e6b
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-08-07 19:29:10 +0530

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -36,4 +36,4 @@ def on_stream_end(chat_id: int) -> None:
         pytgcalls.change_stream(
             chat_id, queues.get(chat_id)["file"]
         )
-run = pytgcalls.run
+call_py = pytgcalls.start()
